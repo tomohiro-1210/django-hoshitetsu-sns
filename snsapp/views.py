d36986a7ebe8f1edf3e7ac4fd9d25cc34f485f3d
tomohiro_0225 <express22212270@gmail.com>
2024-08-28 17:45:36 +0900

    ユーザーテスト登録 データの重複があったときの処理

--- snsapp/views.py
+++ snsapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Djangoが準備してくれているユーザーモデルを読み込み
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def index(request):
@@ -9,14 +10,20 @@ def index(request):
 
 # 新規登録画面
 def signupfunc(request):
+    # ページのデータ
+    template = 'signup.html'
+        
+    # 登録フォームから受け取ったときの処理
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        user = User.objects.create_user(username, email, password)
+        try:
+            user = User.objects.create_user(username, email, password) 
+        except IntegrityError:
+            data = {'error':'このユーザーは既に登録されています'}
+            return render(request, template, data)
     
-    # ページのデータ
-    template = 'signup.html'
     data = {}
     return render(request, template, data)
 
